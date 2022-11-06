f40df30e117d22cd3dc397036e5968d15db9f68f
Frederico Leles Santos <fred.leles@hotmail.com>
2022-11-06 10:38:03 -0300

    [feat]: Implementa o serviço CREATE de produtos

---
+++ Back-End/src/services/ProductsServices.ts
@@ -0,0 +1,14 @@
+import { ProductsZodSchema, IProduct } from '../models/IProduct';
+import BaseServices from './BaseServices';
+
+export default class ProductsServices extends BaseServices<IProduct> {
+  create(obj:unknown) : Promise<IProduct> {
+    // validação dos dados do usuário
+    const parsed = ProductsZodSchema.safeParse(obj);
+
+    if (!parsed.success) {
+      throw parsed.error;
+    }
+    return this._model.create(parsed.data);
+  }
+}
