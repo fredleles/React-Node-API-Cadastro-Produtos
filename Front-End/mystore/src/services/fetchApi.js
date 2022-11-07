4f20068832577ea11d6c776d9c38d32793aa321c
Frederico Leles Santos <fred.leles@hotmail.com>
2022-11-07 09:23:50 -0300

    [feat] Implementa o axios para comunicação com o backend

---
+++ Front-End/mystore/src/services/fetchApi.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: `http://${process.env.REACT_APP_HOSTNAME}:${process.env.REACT_APP_API_PORT}`,
+});
+
+// export const setToken = (token) => {
+//   api.defaults.headers.common.Authorization = token;
+// };
+
+export const requestGet = async (endpoint) => {
+  const { data } = await api.get(endpoint);
+  return data;
+};
+
+export const requestPost = async (endpoint, body) => {
+  const { data } = await api.post(endpoint, body);
+  return data;
+};
+
+export default api;
