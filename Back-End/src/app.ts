c148b5af277ff4ac0fa743bcb82835d941170549
Frederico Leles Santos <fred.leles@hotmail.com>
2022-11-07 09:16:51 -0300

    [feat]: Adiciona cors no backend

--- Back-End/src/app.ts
+++ Back-End/src/app.ts
@@ -2,8 +2,10 @@ import express from 'express';
 import 'express-async-errors';
 import routes from './controllers/routes';
 import errorHandler from './utils/errorsHandler';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 app.use(errorHandler);
