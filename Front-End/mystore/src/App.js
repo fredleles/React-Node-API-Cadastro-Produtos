e953f88e40e8d0c63ac036f61eb3c800236dbc7a
Frederico Leles Santos <fred.leles@hotmail.com>
2022-11-07 11:05:36 -0300

    [feat]: Modifica o App incluir os componentes de exibição dos produtos

--- Front-End/mystore/src/App.js
+++ Front-End/mystore/src/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import Header from './components/Header';
+import ProductDetails from './components/ProductDetails';
+import ProductsList from './components/ProductsList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main>
+        <div className="Main-header">
+          <div><h2>Produtos</h2></div>
+          <div>
+            <button
+              type="button"
+            >
+              +
+            </button>
+          </div>
+        </div>
+        <hr />
+        <section className="Main-content">
+          <div className="Main-content-item">
+            <h5>Lista de Produtos</h5>
+            <ProductsList />
+          </div>
+          <div className="Main-content-item">
+            <h5>Detalhes</h5>
+            <ProductDetails />
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
